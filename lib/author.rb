facf4105ac25d1574629838a5c88fcd0d665461e
Benjamin Jones <bljones36@gmail.com>
2018-09-17 19:16:19 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,25 @@
 class Author
-  attr_accessor :title, :name
+  attr_accessor :name, :posts
+  @@count = 0
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+  
+  def add_song(post)
+    @posts << post
+    song.artist = self
+    @@count += 1
+  end
+
+  def add_song_by_name(post)
+    post = Post.new(post)
+    @posts << post
+    post.author = self
+    @@count += 1
+  end
+  
+  def self.song_count
+    @@count
+  end
 end
